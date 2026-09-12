9caf59abb9a59a458b8afcdce610ba9d2b41da47
Le Tien Thanh <letienthanhthptbs@gmail.com>
2026-09-12 21:09:40 +0700

    remove tailwind, added section, move images into public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   srcDir: "./src/client",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+ 
 });
